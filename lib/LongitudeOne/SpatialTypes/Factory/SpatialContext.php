3ee4935d967799b9517e7a57063662862efc8a73
Alexandre Tranchant <alexandre.tranchant@gmail.com>
2026-08-09 17:52:08 +0200

    feat: SpatialContext introduced

---
+++ lib/LongitudeOne/SpatialTypes/Factory/SpatialContext.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * This file is part of the spatial project.
+ *
+ * PHP 8.4 | 8.5
+ *
+ * Copyright Alexandre Tranchant <alexandre.tranchant@gmail.com> 2024-2026
+ * Copyright Longitude One 2024-2026
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ */
+
+declare(strict_types=1);
+
+namespace LongitudeOne\SpatialTypes\Factory;
+
+use LongitudeOne\SpatialTypes\Enum\DimensionEnum;
+use LongitudeOne\SpatialTypes\Enum\FamilyEnum;
+use LongitudeOne\SpatialTypes\Interfaces\SpatialInterface;
+
+/**
+ * Immutable context used to create a spatial type.
+ */
+final readonly class SpatialContext
+{
+    /**
+     * @param int           $srid      Spatial Reference Identifier
+     * @param FamilyEnum    $family    Spatial family
+     * @param DimensionEnum $dimension Coordinate dimension
+     */
+    public function __construct(
+        public int $srid = SpatialInterface::DEFAULT_SRID,
+        public FamilyEnum $family = FamilyEnum::GEOMETRY,
+        public DimensionEnum $dimension = DimensionEnum::X_Y
+    ) {
+    }
+}
---
+++ tests/LongitudeOne/SpatialTypes/Tests/Unit/Factory/SpatialContextTest.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * This file is part of the spatial project.
+ *
+ * PHP 8.4 | 8.5
+ *
+ * Copyright Alexandre Tranchant <alexandre.tranchant@gmail.com> 2024-2026
+ * Copyright Longitude One 2024-2026
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ */
+
+declare(strict_types=1);
+
+namespace LongitudeOne\SpatialTypes\Tests\Unit\Factory;
+
+use LongitudeOne\SpatialTypes\Enum\DimensionEnum;
+use LongitudeOne\SpatialTypes\Enum\FamilyEnum;
+use LongitudeOne\SpatialTypes\Factory\SpatialContext;
+use LongitudeOne\SpatialTypes\Interfaces\SpatialInterface;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @internal
+ *
+ * @covers \LongitudeOne\SpatialTypes\Factory\SpatialContext
+ */
+class SpatialContextTest extends TestCase
+{
+    /**
+     * Test the default spatial context.
+     */
+    public function testDefaultValues(): void
+    {
+        $context = new SpatialContext();
+
+        static::assertSame(SpatialInterface::DEFAULT_SRID, $context->srid);
+        static::assertSame(FamilyEnum::GEOMETRY, $context->family);
+        static::assertSame(DimensionEnum::X_Y, $context->dimension);
+    }
+
+    /**
+     * Test a customized spatial context.
+     */
+    public function testCustomValues(): void
+    {
+        $context = new SpatialContext(4326, FamilyEnum::GEOGRAPHY, DimensionEnum::X_Y_Z_M);
+
+        static::assertSame(4326, $context->srid);
+        static::assertSame(FamilyEnum::GEOGRAPHY, $context->family);
+        static::assertSame(DimensionEnum::X_Y_Z_M, $context->dimension);
+    }
+}
